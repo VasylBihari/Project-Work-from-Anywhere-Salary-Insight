ca5c76aee03db48204dc6622569adc6089a59af4
VasylBihari <vasylbihari@gmail.com>
2025-05-28 08:26:59 +0300

    Update data_cleaning.sql

--- sql/data_cleaning.sql
+++ sql/data_cleaning.sql
@@ -41,3 +41,19 @@ SELECT
 FROM jobs;
 
 
+/*Створюємо таблицю з курсами валют*/
+
+CREATE TABLE exchange_rates (
+    currency_code TEXT PRIMARY KEY,
+    exchange_rate_to_usd NUMERIC NOT NULL
+);
+
+
+/*ДОДАЄМО курси валют*/
+INSERT INTO exchange_rates (currency_code, exchange_rate_to_usd)
+VALUES ('JPY', 0.0067);
+
+/*Перевіряємо дані в створеній таблиці*/
+select *
+from exchange_rates;
+
